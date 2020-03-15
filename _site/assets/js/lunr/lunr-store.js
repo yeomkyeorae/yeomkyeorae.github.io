647fad28f3574cbae3543140dc036ab5418b71c3
yeomkyeorae <duarufp06@naver.com>
2020-03-15 15:21:50 +0900

    -

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/first-post/",
+        "url": "https://yeomkyeorae.github.io/jekyll/update/first-post/",
         "teaser": null
       },{
         "title": "hihihihi!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/haha-yeom/",
+        "url": "https://yeomkyeorae.github.io/jekyll/update/haha-yeom/",
         "teaser": null
       },{
         "title": "서시 - 윤동주",
         "excerpt":" ","categories": ["윤동주"],
         "tags": [],
-        "url": "http://localhost:4000/%EC%9C%A4%EB%8F%99%EC%A3%BC/si-susi/",
+        "url": "https://yeomkyeorae.github.io/%EC%9C%A4%EB%8F%99%EC%A3%BC/si-susi/",
         "teaser": null
       },{
         "title": "github.io 블로그 시작하기",
         "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13...","categories": ["Blog"],
         "tags": ["Blog"],
-        "url": "http://localhost:4000/blog/test/",
+        "url": "https://yeomkyeorae.github.io/blog/test/",
         "teaser": null
       }]
